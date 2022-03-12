387e569db1d8a35889f3bb03f6dd2977aba2899c
DLineone <DLineone@users.noreply.github.com>
2022-03-13 02:47:10 +0300

    new

---
+++ script.js
@@ -0,0 +1,13 @@
+const questions = document.querySelector(".questions");
+const positiveAnsver = document.querySelector(".positive-ansver");
+const negativeAnsver = document.querySelector(".negative-ansver");
+const addProperty = document.querySelector(".add-property");
+const addObject = document.querySelector(".add-object");
+
+
+(function init()
+{
+    questions.querySelector("*").style.display = "flex";
+    console.log("Log: 1");
+    console.log(questions);
+}())
